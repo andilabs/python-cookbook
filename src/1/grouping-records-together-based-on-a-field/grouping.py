a16a4388d7270aa43ff9dc6e1dcdadea9be48417
Andrzej Kostanski <andrzej.kostanski@gmail.com>
2016-11-22 14:49:25 +0100

    itertools groupby

--- src/1/grouping-records-together-based-on-a-field/grouping.py
+++ src/1/grouping-records-together-based-on-a-field/grouping.py
@@ -1,3 +1,19 @@
+"""
+Make an iterator that returns consecutive keys and groups from the iterable. 
+The key is a function computing a key value for each element. 
+If not specified or is None, key defaults to an identity function and returns the element unchanged. 
+Generally, the iterable needs to already be sorted on the same key function.
+
+The operation of groupby() is similar to the uniq filter in Unix. 
+It generates a break or new group every time the value of the key function changes
+(which is why it is usually necessary to have sorted the data using the same key function). 
+That behavior differs from SQL’s GROUP BY which aggregates common elements regardless of their input order.
+
+The returned group is itself an iterator that shares the underlying iterable with groupby(). 
+Because the source is shared, when the groupby() object is advanced, the previous group is no longer visible. 
+So, if that data is needed later, it should be stored as a list:
+"""
+
 rows = [
     {'address': '5412 N CLARK', 'date': '07/01/2012'},
     {'address': '5148 N CLARK', 'date': '07/04/2012'},
@@ -10,6 +26,8 @@ rows = [
 ]
 
 from itertools import groupby
+# https://docs.python.org/3/library/itertools.html#itertools.groupby
+
 
 rows.sort(key=lambda r: r['date'])
 for date, items in groupby(rows, key=lambda r: r['date']):
